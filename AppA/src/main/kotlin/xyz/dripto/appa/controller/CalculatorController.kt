a15b89bd69189346a9aed17e6194410df575c9e0
Driptaroop Das <driptaroop.das@gmail.com>
2020-06-22 16:37:22 +0200

    guide

--- AppA/src/main/kotlin/xyz/dripto/appa/controller/CalculatorController.kt
+++ AppA/src/main/kotlin/xyz/dripto/appa/controller/CalculatorController.kt
@@ -12,6 +12,10 @@ import org.springframework.web.client.getForObject
 class CalculatorController(
         val restTemplate: RestTemplate
 ) {
+    /**
+     * Guides: http://www.vinsguru.com/spring-boot-distributed-tracing-with-jaeger/
+     * https://medium.com/@klaus.dobbler/introducing-distributed-tracing-to-a-docker-swarm-landscape-f92c033e36db
+     */
     @GetMapping("/multiply/{num}")
     fun multiplyBy(@PathVariable("num") i: Int)
             = restTemplate.getForObject<Int>("http://localhost:8082/appb/multiply/${i*2}")
